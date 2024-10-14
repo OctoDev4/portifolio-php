dff1982be5749ccdf51074d5fc1c488fec06ec38
Felipe Almeida <felipe.alhopato@gmail.com>
2024-10-13 21:07:54 -0300

    arrays and objects

--- index.php
+++ index.php
@@ -3,66 +3,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portifolio</title>
+    <title>Portfólio</title>
 </head>
 <body>
 
-<?php 
+<?php
 
-  $welcome = "Oi";
-  $name = 'Louco';
-  $title = $welcome . ", Portifolio do " . $name;
-  $subTitle = "seja bem vindo";
-  $yearNow = date("Y");
-  $project = "portifolio";
-  $isFinished = true; 
-  $projectDate = date("d/m/Y H:i:s");
-  $description = "meu primeiro portifolio escrito em PHP"
+$welcome = "Oi";
+$name = 'Louco';
+$title = $welcome . ", Portfólio do " . $name;
+$subTitle = "seja bem vindo";
+$yearNow = date("Y");
+
+$projects = [
+    (object) [
+        'name' => 'Portfólio',
+        'description' => 'meu primeiro portfólio escrito em PHP',
+        'isFinished' => true,
+        'date' => date("d/m/Y H:i:s"),
+    ],
+    // Você pode adicionar mais projetos aqui como novos objetos
+];
 
 ?>
 
 <h1>
-<?php
-  echo $title;
-?>
+    <?php echo $title; ?>
 </h1>
 
 <p>
-<?php
-  echo $subTitle;
-?>
+    <?php echo $subTitle; ?>
 </p>
 
 <p>
-<?php
-  echo $yearNow;
-?>
+    <?php echo $yearNow; ?>
 </p>
 <hr>
-<div>
-    <h2><?=$project?></h2>
-    <p><?=$description?></p>
 
-
-    <div
-
-    <?php if((2023 - $yearNow) > 2): ?>
-        style='background-color: burlywood;'
-    <?php endif   ?>
-    >
-      
-    <div><?= $projectDate ?></div>
-
+<?php foreach ($projects as $project): ?>
     <div>
-       <?php if($isFinished): ?>
-        <span>Finalizado ✅</span>
-       <?php else: ?>
-        <span>Em andamento ⏳</span>
-       <?php endif;?>
-     </div>
+        <h2><?= $project->name ?></h2>
+        <p><?= $project->description ?></p>
 
+        <div <?php if((2023 - $yearNow) > 2): ?> style='background-color: burlywood;' <?php endif; ?>>
+            <div><?= $project->date ?></div>
+
+            <div>
+                <?php if($project->isFinished): ?>
+                    <span>Finalizado ✅</span>
+                <?php else: ?>
+                    <span>Em andamento ⏳</span>
+                <?php endif; ?>
+            </div>
+        </div>
     </div>
-</div>
-    
+<?php endforeach; ?>
+
 </body>
 </html>
